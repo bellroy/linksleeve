9d9206827c04d6eb31aad039b7071e8beb4f9895
matt <matt@dec4ba91-d232-0410-a499-b0f1e7490611>
2008-05-13 01:14:42 +0000

    bugfix: incorrect XMLRPC call
    git-svn-id: http://code.trike.com.au/svn/rails/plugins/link_sleeve@122 dec4ba91-d232-0410-a499-b0f1e7490611

--- lib/link_sleeve.rb
+++ lib/link_sleeve.rb
@@ -3,10 +3,17 @@ require 'xmlrpc/client'
 
 class LinkSleeve
   def self.spam?(text)
-    server.call("slv", text) == 0
+    begin
+      server.call("slv", text) == 0
+    rescue RuntimeError => e
+      if defined?(RAILS_DEFAULT_LOGGER)
+        RAILS_DEFAULT_LOGGER.warn "LinkSleeve down?: '#{e.message}'. Allowing post anyway.\n<<COMMENT\n#{text}\nCOMMENT"
+      end
+      return false
+    end
   end
 
   def self.server
-    XMLRPC::Client.new("www.linksleeve.org", "slv.php")
+    XMLRPC::Client.new("www.linksleeve.org", "/slv.php")
   end
 end
--- test/link_sleeve_test.rb
+++ test/link_sleeve_test.rb
@@ -12,7 +12,7 @@ class LinkSleeveTest < Test::Unit::TestCase
     server = stub(:call => 1)
     XMLRPC::Client.stubs(:new).returns(server)
 
-    assert !LinkSleeve.spam?("Not spam.")
+    deny LinkSleeve.spam?("Not spam.")
   end
   def test_that_correct_arguments_are_passed_to_xmlrpc_server
     server = stub()
@@ -23,8 +23,24 @@ class LinkSleeveTest < Test::Unit::TestCase
   end
   def test_that_correct_xmlrpc_server_is_initialized
     server = stub_everything()
-    XMLRPC::Client.expects(:new).with("www.linksleeve.org","slv.php").returns(server)
+    XMLRPC::Client.expects(:new).with("www.linksleeve.org","/slv.php").returns(server)
 
     LinkSleeve.spam?("A string.")
+  end
+  def test_that_link_sleeve_errors_dont_stop_posting
+    server = stub()
+    server.stubs(:call).raises(RuntimeError)
+    XMLRPC::Client.stubs(:new).returns(server)
+
+    deny LinkSleeve.spam?("A string")
+  end
+
+  private
+
+  def deny(boolean, message = nil)
+    message = build_message message, '<?> is not false or nil.', boolean
+    assert_block message do
+      not boolean
+    end
   end
 end
